7e4b8991ee41d5934c507211c2b8f60c7cdb17ff
Ankit <ankit0525252@gmail.com>
2025-08-28 21:25:14 +0530

    ping-pong

--- websocket/src/index.ts
+++ websocket/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hi")
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (socket) => {
+  socket.on("message", (e)=>{
+    if(e.toString() === 'ping'){
+      socket.send("pong");
+    }
+  })
+});
